0bb565d0f3baa7af400757acdc5d07d737d316eb
briannanjeri <briannanjeri2020@gmail.com>
2023-12-04 20:26:40 +0300

    feat:add conditional rendering of employer dashboard

--- codsoft/src/components/employer/dashboard/Dashboard.js
+++ codsoft/src/components/employer/dashboard/Dashboard.js
@@ -3,11 +3,16 @@ import HeroSection from "./hero-section";
 import { Statistics } from "./statistics";
 import { PostedJobList } from "./postedJobList";
 const Dashboard = () => {
+  const employerToken=localStorage.getItem('employerToken')
   return (
     <div>
+      { !employerToken &&
+      <div>
       <HeroSection />
       <PostedJobList />
       {/* <Statistics /> */}
+      </div>
+      }
     </div>
   );
 };
